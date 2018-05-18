aa689ee7cf1457c094aa762bbaacf4f59ce46ea1
Alex Sell <alextricityy@gmail.com>
2018-05-18 15:29:13 +0100

    heroku>

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root_path :cocktails_path
-  root to: "/cocktails"
+  root to: "cocktails#index"
   resources :cocktails do
   # references :cocktail
   resources :doses, only: [:new, :create]
